0d5b99bf73a0a4d09b0385c4dd6b2f6c1d86f9f3
gauravkharel <kharel619@gmail.com>
2022-11-21 12:23:00 +0545

    routes implemented

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
  import express from 'express'
-
+ import router from './router'
  const app = express()
 
  app.get('/', (req, res) => {
@@ -7,6 +7,8 @@
   res.status(200)
   res.json({message: "hello"})
  })
+
+ app.use('/api', router)
 
  export default app
  
